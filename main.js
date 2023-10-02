c88fe094d3b1c6a011986e0a98b8f319d4620741
Vitalii Bukatin <bukatin93@gmail.com>
2023-10-02 11:56:37 +0300

    completed hw

---
+++ main.js
@@ -0,0 +1,18 @@
+// Написати функцію, яка приймає 1 параметр. з тим, що передали перший раз і т. д. Все це із замиканнями, наприклад:
+// sum(3) = 3 
+// sum(5) = 8
+// sum(20) = 28
+
+let sum = (function () {
+	let result = 0;
+	return function (x) {
+		return result += x;
+	}
+})();
+
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));
+console.log(sum(100));
+
+
